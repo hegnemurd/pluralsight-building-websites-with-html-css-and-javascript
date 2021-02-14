38a5105de1b78022750518f93a9e637953d8aa27
hegnemurd <hegne.murd@gmail.com>
2021-02-14 11:42:44 +0000

    Getting Directions With Google Maps

---
+++ working-with-geolocation-in-html-by-paul-d-sheriff/bethanys-pie-shop-website-8/getting-directions-with-google-maps/scripts/loadGoogleMaps.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function loadGoogleMaps() {
+  // Set your Google Maps key here
+  let key = "YOUR_KEY_HERE";
+  // Set your callback function here
+  let callback = "gmapController.initialize";
+
+  // Create a new <script> element
+  let elem = document.createElement("script");
+  elem.type = "text/javascript";
+  elem.src =
+    "https://maps.googleapis.com/maps/api/js?key=" +
+    key +
+    "&callback=" +
+    callback;
+  document.body.appendChild(elem);
+}
